a32aae135c003b9f791b60d575a2aee7117831c5
Daniel <oziegbeosy@gmail.com>
2023-02-17 15:20:13 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,14 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
-	{
+		{
 		putchar(a + '0');
-	if (a < 9)
+		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	}
+		}
 	putchar('\n');
 	return (0);
 }
-
