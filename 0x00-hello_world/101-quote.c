851204d1d21aee45255ec41cd09f98b20ecd0fd9
Daniel <oziegbeosy@gmail.com>
2023-02-16 18:05:58 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /*
- * main is Entry
- * Return 1
+ * main is Entry point
+ * Return; 1
  */
 int main(void)
 {
-    char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    char *msg = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
     write(2, msg, 59);
     return 1;
 }
