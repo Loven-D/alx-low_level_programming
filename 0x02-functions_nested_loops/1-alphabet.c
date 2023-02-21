b3b555739dd80be303dab6595f495ed4370e3e6d
Daniel <oziegbeosy@gmail.com>
2023-02-21 06:59:24 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Program that prints alphabet in lowercase
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	return (0);
+}
