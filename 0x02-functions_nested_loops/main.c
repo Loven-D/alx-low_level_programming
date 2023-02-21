8c7c0488274c4569464cada124f78767b526c978
Daniel <oziegbeosy@gmail.com>
2023-02-21 13:27:48 -0800

    task 2

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -3,11 +3,11 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: always 0.
  */
+
 int main(void)
 {
-	print_alphabet();
-	_putchar('\n');
+	print_alphabet_x10();
 	return (0);
 }
