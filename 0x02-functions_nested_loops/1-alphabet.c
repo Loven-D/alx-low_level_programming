f9f23bafa6c5d95657986d92541695d92e861acc
Daniel <oziegbeosy@gmail.com>
2023-02-21 08:09:46 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,15 +8,15 @@
  * Return: 0 (Success)
  */
 
+int _putchar(char c);
 void print_alphabet(void)
 {
     char c = 'a';
 
     while (c <= 'z') {
-        _putchar(c);
+        _putchar(c,'\n');
         c++;
     }
     
-    _putchar('\n');
 }
 
