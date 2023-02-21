59002284608e8601a83e204d3a9fd0d073de5f13
Daniel <oziegbeosy@gmail.com>
2023-02-21 09:02:17 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,9 @@
 
 /**
  * print_alphabet - print the alphabet in lower case
- * followed by a new line
- * Return: 0 (Success)
+  * Return: 0 (Success)
 */
+
 void print_alphabet(void)
 {
 	char c = 'a';
