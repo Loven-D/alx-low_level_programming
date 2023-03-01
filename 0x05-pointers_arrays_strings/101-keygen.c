65cdf6296bd888250e16d574ac88389a42cbc7b7
Daniel <oziegbeosy@gmail.com>
2023-03-01 03:44:47 -0800

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a random valid password for 101-crackme
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const int charset_size = sizeof(charset) - 1;
+	char password[6];
+	int i, index;
+	
+	srand(time(NULL));
+	
+	for (i = 0; i < 5; i++)
+	{
+		index = rand() % charset_size;
+		password[i] = charset[index];
+	}
+	password[5] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
