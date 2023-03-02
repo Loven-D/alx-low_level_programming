646df5b0bee95639ad91260a592293779b83ebb7
Daniel <oziegbeosy@gmail.com>
2023-03-02 13:39:18 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,29 +2,37 @@
 
 /**
  * cap_string - Capitalize all words of a string
- * @str: input string to be capitalized
+ * @str: input to capitalize
  *
- * Return: capitalized string
+ * Return: capitalized string.
  */
 char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i -1] == '\t' ||
-				str[i - 1] == '\n' || str[i - 1] == ',' ||
-				str[i - 1] == ';' || str[i - 1] == '.' ||
-				str[i - 1] == '!' || str[i -1] == '?' ||
-				str[i - 1] == '\"' || str[i - 1] == '(' ||
-				str[i - 1] == ')' || str[i - 1] == '{' ||
-				str[i - 1] == '}')
+		if ((str[i - 1] == ' ' || str[i - 1] == '\n'
+		|| str[i - 1] == '\t' || str[i - 1] == ','
+		|| str[i - 1] == ';' || str[i - 1] == '!'
+		|| str[i - 1] == '?' || str[i - 1] == '"'
+		|| str[i - 1] == '(' || str[i - 1] == ')'
+		|| str[i - 1] == '{' || str[i - 1] == '}'
+		|| str[i - 1] == '.')
+		&& (str[i] >= 'a' && str[i] <= 'z'))
 		{
-			if (str[1] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-			}
+			str[i] = str[i] - 32;
 		}
+		else if ((str[0] >= 97 && str[0] <= 122))
+		{
+			str[0] = str[0] - 32;
+		}
+		else
+		{
+			str[i] = str[i];
+		}
+		i++;
 	}
 	return (str);
 }
