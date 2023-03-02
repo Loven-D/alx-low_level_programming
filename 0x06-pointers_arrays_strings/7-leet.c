d3254e8b863648e333168cf315e0a024d33c5edb
Daniel <oziegbeosy@gmail.com>
2023-03-02 14:22:34 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * leet -  encodes a string into 1337.
+ * @s: string to code
+ * Return: the enconde string.
+ */
+char *leet(char *str)
+{
+	int i;
+	int j;
+	int k;
+
+	char l[] = "aAeEoOtTlL";
+	char e[] = "4433007711";
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
+		{
+			if (str[i] == l[j])
+			{
+				k = j;
+				str[i] = e[k];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
