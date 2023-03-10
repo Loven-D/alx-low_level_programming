cc28aa675f445dee2d4f4925f189592e8b2bb93a
Daniel <oziegbeosy@gmail.com>
2023-03-10 15:54:35 -0800

    Argv and Argc Task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: either 1 or 0 always.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argv != 3)
+	{
+		printf ("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	result = a * b;
+
+	printf ("%d\n", result);
+	return(1);
+}
