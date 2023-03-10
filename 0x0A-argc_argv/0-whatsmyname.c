4d073ca351f5ba7e095e54c35428ba225df23dde
Daniel <oziegbeosy@gmail.com>
2023-03-10 14:07:22 -0800

    Argc and Argv Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: an arrays of pointers to the argument
  * Return: 0 always.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
