41541c3f5118de0482f98d9351e6b544a7d05f75
Daniel <oziegbeosy@gmail.com>
2023-03-10 16:18:25 -0800

    Argv and Argc Task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,13 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf ("Error\n");
+		printf("Error\n");
 		return (1);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	result = a * b;
-	
-	printf ("%d\n", result);
-	
+
+	printf("%d\n", result);
 	return (0);
 }
