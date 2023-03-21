8af77d529fa1610d4e7cff0aa8a6b1949295f99c
Daniel <oziegbeosy@gmail.com>
2023-03-21 12:45:27 -0700

    struct task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,8 +19,9 @@ struct dog
 
 typedef struct dog dog_t;
 
-void dog_name(struct dog d);
-float dog_age(struct dog d);
-void dog_owner(struct dog *d, char *new_owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
