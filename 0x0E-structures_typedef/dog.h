6ab263721f227208f377f56fe44beb84494a5d7e
Daniel <oziegbeosy@gmail.com>
2023-03-20 21:16:33 -0700

    struct task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,22 @@
 
 #define DOG_H
 
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+
+typedef struct dog dog_t;
 
 void dog_name(struct dog d);
 float dog_age(struct dog d);
