d2d78b54f673253b691353384eb673a6a52004eb
Daniel <oziegbeosy@gmail.com>
2023-03-30 14:12:58 -0700

    prints all elements of list_t

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
 		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
-		
+
 		count++;
 		h = h->next;
 	}
