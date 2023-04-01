ee474eb1151e4c28f8ad009edb7f7a46f08605fe
Daniel <oziegbeosy@gmail.com>
2023-04-01 16:21:21 -0700

    function to print a statement before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 
+void sudo_main(void)__attribute__((constructor));
+
 /**
  * sudo_main - function to print before the main
  */
 void sudo_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
-}
-
-/**
- * main - entry of the main code
- * Return: always 0
- */
-int main(void)
-{
-	sudo_main();
-
-	return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
