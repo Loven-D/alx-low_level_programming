e47d88bb888005bad8b5c738462e5e22b20e3294
Daniel <oziegbeosy@gmail.com>
2023-04-04 17:25:02 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes the node at the given index of a listint_t linked list
+ * @head: Pointer to a pointer to the head of the list
+ * @index: Index of the node to be deleted, starting at 0
+ *
+ * Return: 1 if deletion succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *current, *temp;
+
+	if (*head == NULL) /* Check if list is empty */
+		return (-1);
+
+	current = *head; /* Set current to the head of the list */
+
+	/* If index is 0, delete the head node */
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+
+	/* Traverse the list to the node before the desired index */
+	for (i = 0; i < index - 1 && current != NULL; i++)
+		current = current->next;
+
+	/* If the index is out of bounds, return -1 */
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	temp = current->next; /* Store the node to be deleted in a temporary variable */
+	current->next = current->next->next; /* Update the next pointer of the current node */
+
+	free(temp); /* Free the memory of the deleted node */
+	return (1);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t linked list
+ * @head: Pointer to a pointer to the head of the list
+ * @idx: Index at which the new node should be added, starting at 0
+ * @n: Value of the new node to be added
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *current, *new_node;
+
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n; /* Set the data value of the new node */
+	current = *head; /* Set current to the head of the list */
+
+	/* If idx is 0, insert new node as the new head of the list */
+	if (idx == 0)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return new_node;
+	}
+
+	/* Traverse the list to the node before the desired index */
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
+
+	/* If the index is out of bounds, free the new node and return NULL */
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Insert the new node after the node at the desired index */
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
+}
