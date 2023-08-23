90e967f0c704d016512047c160202cb7546534f0
Oziegbe Daniel <oziegbeosy@gmail.com>
2023-08-23 15:27:30 -0700

    task 5 update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
+"""Module that calculates the perimeter of an island in a grid."""
 
-#function that returns the perimeter of the island described in grid
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in grid.
-    
-    :param grid: A list of list of integers representing the island.
-    :return: The perimeter of the island.
-    """
-    if not grid:
-        return (0)
+    """Returns the perimeter of the island in grid."""
 
-    perimeter = 0
-    rows, cols = len(grid), len(grid[0])
-    
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4  # Start with 4 sides
-                
-                # Check left neighbor
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2  # Subtract 2 for shared edge
-                
-                # Check top neighbor
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2  # Subtract 2 for shared edge
-    
-    return perimeter
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perim += num_water_neighbors(grid, i, j)
+
+    return perim
